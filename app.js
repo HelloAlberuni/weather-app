1500ebc24516e8a19f317f9605bb63e5eb86f35e
Azad <azad@hastechit.com>
2021-08-24 19:50:46 +0600

    .

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const weather = new Weather();
+const ui = new UI();
 
 // Make HTTP Request
-weather.getData()
-.then(function(data){
-    
+let request = weather.getData();
+
+request.then(function(data){
+    // update weather based on current location
+    console.log(data);
+    ui.updateWeather();
 });
 
-// const ui = new UI();
 
-// update weather based on current location
-// ui.updateWeather();
+
+
 
 // update weather when user changed location
 // ui.updateWeather(data);
