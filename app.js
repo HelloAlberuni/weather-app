7bcba53bc28e769033b67af83bd54e5570084df9
Azad <azad@hastechit.com>
2021-08-25 20:01:49 +0600

    .

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ let request = weather.getData();
 
 request.then(function(data){
     // update weather based on current location
+    let dateNow = new Date(),
+        days = ['Sunday', 'Monday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+    let today = {};
+    today.day = days[dateNow.getDay()];
+    today.date = dateNow.getDate();
+    today.month = months[dateNow.getMonth()].slice(0, 3);
+    today.year = dateNow.getFullYear();
+
+
+    // ui.updateWeather();
+
     console.log(data);
-    ui.updateWeather();
 });
 
 
--- weather.js
+++ weather.js
@@ -2,13 +2,21 @@ class Weather{
     constructor(){
         this.city = 'dhaka';
         this.apiKey = '4b45462b359fda05021d1bb246b299e6';
+        this.daysCount = 4;
     }
 
     async getData(){
-        let response = await fetch(`//api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`);
+        let currentWeatherResponse = await fetch(`//api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}`);
+        console.log(currentWeatherResponse);
         // console.log(response);
-        let weatherData = await response.json();
+        let currentWeather = await currentWeatherResponse.json();
 
-        return weatherData;
+        // let next3DaysResponse =  await fetch(`//api.openweathermap.org/data/2.5/forecast/daily?q=${this.city}&cnt=${this.daysCount}&appid=${this.apiKey}`);
+        // let next3DaysWeather = await next3DaysResponse.json();
+
+        return {
+            currentWeather,
+            // next3DaysWeather
+        };
     }
 }
